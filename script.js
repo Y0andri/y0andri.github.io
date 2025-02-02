e15607656a1fc3ca505fd163c8a0fb3ac77bb434
Y0andri <yoanpedro20@gmail.com>
2025-02-01 21:02:59 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
+    document.addEventListener("DOMContentLoaded", function() {
     fetch('componentes/header.html')
         .then(response => response.text())
         .then(data => {
@@ -17,7 +17,33 @@ document.addEventListener("DOMContentLoaded", function() {
         .then(response => response.text())
         .then(data => {
             document.getElementById('footer').innerHTML = data;
-        });
+        });   
+        
+        
+        // Comprobar si ya se ha preguntado el nombre del usuario
+    if (!localStorage.getItem('nombreUsuario')) {
+        // Preguntar el nombre del usuario
+        var nombre = prompt('Hola ¿Como te llamas?');
+        
+        // Guardar el nombre en localStorage
+        localStorage.setItem('nombreUsuario', nombre);
+        
+        // Seleccionar el elemento por su ID
+        var elemento = document.getElementById('user_name');
+        
+        // Mostrar el nombre del usuario en el elemento
+        if (nombre) {
+            elemento.innerHTML = 'Bienvenido, ' + nombre + '!';
+        } else {
+            elemento.innerHTML = 'Hola, visitante!';
+        }
+    } else {
+        // Si ya se ha guardado el nombre, mostrarlo directamente
+        var nombreGuardado = localStorage.getItem('nombreUsuario');
+        var elemento = document.getElementById('user_name');
+        elemento.innerHTML = 'Hola de nuevo, ' + nombreGuardado + '!';
+    }
+
 });
 
 
