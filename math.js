d3ee6d05fe8de14e8bf8be3f0eb2e6a019cd1b0d
Y0andri <yoanpedro20@gmail.com>
2025-03-29 00:46:34 -0400

    Add files via upload

---
+++ math.js
@@ -0,0 +1,72 @@
+
+const outResultado = document.getElementById('resultado');
+const btnPendiente = document.getElementById('btn_pendiente');
+const btnPuntoMedio = document.getElementById('btn_puntomedio'); 
+const btnLongitud = document.getElementById('btn_longitud');
+
+const inputs = ()=>{
+    aX = document.getElementById('aX').value;
+    aY = document.getElementById('aY').value;
+    bX = document.getElementById('bX').value;
+    bY = document.getElementById('bY').value;
+}
+
+const pendiente = ()=>{
+    inputs();
+    while (!aX || !aY || !bX || !bY) {
+        return "rellena todos los campos";
+    }
+    let y = aY - bY;
+    let x = aX - bX;
+    if(x == 0) {
+    return "no tiene solución";
+    }
+    if(true == Number.isInteger(y / x)) {
+    return y / x ;
+    } 
+    else {
+    return y + "/" + x ;
+    }
+}
+
+const longitud = ()=>{
+    inputs();
+    while (!aX || !aY || !bX || !bY) {
+        return "rellena todos los campos";
+    }
+    let y = aY - bY;
+    let x = aX - bX;
+    let sDeP = Math.pow(x,2) + Math.pow(y,2);
+    if(true == Number.isInteger(Math.sqrt(sDeP))) {
+    return Math.sqrt(sDeP);
+    }
+    else {
+    return "√" + sDeP ;
+    }
+}
+
+const puntoMedio = ()=>{
+    inputs();
+    while (!aX || !aY || !bX || !bY) {
+        return "rellena todos los campos";
+    }
+    let y = (aY + bY) / 2;
+    let x = (aX + bX) / 2;
+    return `( ${x} ; ${y} )`;
+}
+
+
+
+btnPendiente.addEventListener('click', function(event){
+    outResultado.innerText = pendiente();
+})
+btnPuntoMedio.addEventListener('click', function(event){
+    outResultado.innerText = puntoMedio();
+})
+btnLongitud.addEventListener('click', function(event){
+    outResultado.innerText = longitud();
+})
+
+
+
+
