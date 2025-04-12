d1f2715e59197c565660d75df009cd7ae60358b2
Y0andri <yoanpedro20@gmail.com>
2025-04-12 19:39:41 -0400

    Add files via upload

--- js/genHTML.js
+++ js/genHTML.js
@@ -1,10 +1,31 @@
-let formBlog = document.getElementById('form_blog') ;
+const boton = document.querySelector('.btn-gen-html');
+const resultadoDiv = document.querySelector('.resultado-codigo');
 
-formBlog.addEventListener('submit', function(event) {
-    event.preventDefault();
-    let nombre = document.getElementById('nombre').value ;
-    let texto = document.getElementById('texto').value ;
-
-    let textArea = document.getElementById('areatext');
-    textArea.innerText = `El nombre es ${nombre} y el texto es ${texto}` ;
-} )
+boton.addEventListener("click", ()=>{
+const nombre = document.getElementById("nombre").value;
+const imagen = document.getElementById("imagen").value;
+const mensaje = document.getElementById("mensaje").value;
+    
+    if(nombre == "" || mensaje == "") {
+        resultadoDiv.innerText = "Tanto el nombre como el mensaje deben rellenarse"
+    }
+    else {
+        let imagenTag ;
+        
+        if (imagen == ""){
+            imagenTag = "" ;
+        }
+        else {
+            imagenTag = `<img src="${imagen}">` ;
+        }
+        
+        let mensajeBr = mensaje.replaceAll('\n', "<br>");
+        
+        let codigo = `<div ${nombre}>${imagenTag}
+        ${mensajeBr}
+        </div>
+        `
+        resultadoDiv.innerText = codigo ;
+        
+    }
+})
