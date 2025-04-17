3193804b71f24cf56f36cff9c31aa91d39a15da1
Y0andri <yoanpedro20@gmail.com>
2025-04-17 17:03:31 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+  const body = document.querySelector("body");
+  
+  
+  
   // Inicializador de Tema
   let tema = localStorage.getItem("tema");
 if(localStorage.getItem("tema")) {
@@ -5,7 +9,35 @@ if(localStorage.getItem("tema")) {
     }
     
     
-
+    
+    
+    
+    
+    
+    
+    //Chrome 50 Compatibility
+    
+    function ponerTema(tEma){
+        body.className = " ";
+        body.classList.add(tEma);
+        localStorage.setItem("tema", tEma);
+        themeBg.classList.toggle("desactivado");
+        body.classList.remove("non-scroll");
+    }
+    
+    const lightAlt = document.querySelector(".btn-light-alt");
+    const darkAlt = document.querySelector(".btn-dark-alt");
+    const dbzAlt = document.querySelector(".btn-dbz-alt");
+    
+    lightAlt.addEventListener('click', function(){
+        ponerTema("Light");
+    } )
+    darkAlt.addEventListener('click', function(){
+        ponerTema("Dark");
+    } )
+    dbzAlt.addEventListener('click', function(){
+        ponerTema("Dbz");
+    } )
     
     
 
