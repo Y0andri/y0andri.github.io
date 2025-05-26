b77244f463b2b6c9fef48f1c1434e7e8c79c0305
Y0andri <yoanpedro20@gmail.com>
2025-05-26 17:34:56 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
   const SUPABASE_URL = 'https://pnsbpdksuateqsvwdyld.supabase.co';
-        const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBuc2JwZGtzdWF0ZXFzdndkeWxkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI2NjA0ODcsImV4cCI6MjA1ODIzNjQ4N30.jJwBcM31Zw42PH5HrKxiSpxJsgFwZEeaTnYYUvKa2Ag';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBuc2JwZGtzdWF0ZXFzdndkeWxkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI2NjA0ODcsImV4cCI6MjA1ODIzNjQ4N30.jJwBcM31Zw42PH5HrKxiSpxJsgFwZEeaTnYYUvKa2Ag';
         window.supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
         while(supabase == undefined){
             setTimeout(()=>{
@@ -8,45 +8,55 @@
                 console.log("intento");
             },3000);
         }
+        
+        // Verifica al cargar la página
+supabase.auth.onAuthStateChange((event, session) => {
+  if (session) { // Usuario logeado
+    console.log("Sesión activa:", session.user.email);
+    document.getElementById("logout-btn").style.display = "block";
+  } else { // No logeado
+    console.log("No hay sesión");
+  }
+});
 
 
-async function getMyUser(){
-    const { data: { user }, error } = await supabase.auth.getUser();
-    const usuario = user;
-    console.log(usuario);
-    return user;
-}
+// async function getMyUser(){
+//     const { data: { user }, error } = await supabase.auth.getUser();
+//     const usuario = user;
+//     console.log(usuario);
+//     return user;
+// }
 
-async function checkLog(){
-    const usuario = await getMyUser();
-    if(usuario){
-        return;
-    } else {
-    if(["/","/index.html","/log.html","/sign.html"].includes(location.pathname)) {
-        console.log("sitio publico");
-    } else {
-        if(!user) window.location.href = "log.html";
-    }
-    }
-}
+// async function checkLog(){
+//     const usuario = await getMyUser();
+//     if(usuario){
+//         return;
+//     } else {
+//     if(["/","/index.html","/log.html","/sign.html"].includes(location.pathname)) {
+//         console.log("sitio publico");
+//     } else {
+//         if(!user) window.location.href = "log.html";
+//     }
+//     }
+// }
 
-checkLog();
+// checkLog();
 
 
 
 document.addEventListener("DOMContentLoaded",function(){
     
     
-const btnLogout = document.querySelector(".logout-btn");
+// const btnLogout = document.querySelector(".logout-btn");
 
-btnLogout.addEventListener("click", async ()=>{
-    const { error } = await supabase.auth.signOut();
-    if(error){
-        console.log("Error al cerrae sesion");
-        return;
-    }
-    console.log("sesion cerrada");
-});
+// btnLogout.addEventListener("click", async ()=>{
+//     const { error } = await supabase.auth.signOut();
+//     if(error){
+//         console.log("Error al cerrae sesion");
+//         return;
+//     }
+//     console.log("sesion cerrada");
+// });
 
 
 const body = document.querySelector("body");
