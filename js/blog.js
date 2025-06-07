927c7bf73af8a5a465a5d7e37b0c53e518203e86
Y0andri <yoanpedro20@gmail.com>
2025-06-07 01:11:05 -0400

    Add files via upload

---
+++ js/blog.js
@@ -0,0 +1,155 @@
+const keyImgBb = "42c2035207510686c7c9d6f3301059b3";
+const keyBin = "6843670a8561e97a50207460";
+const xMasterKey = "$2a$10$wvBBvlgwlGvRYa9Fxhn2peWhelUGxVHKEmhBXVpzJJg3x6oSnqDM.";
+
+// Obtener productos desde la API (reintenta cada 3s si falla)
+const getArticlesPromise = async () => {
+    while (true) {
+        try {
+            const response = await fetch(`https://api.jsonbin.io/v3/b/${keyBin}`, {
+                headers: {
+                    'X-Master-Key': xMasterKey,
+                    'Content-Type': 'application/json'
+                }
+            });
+            const data = await response.json();
+            console.log("Promesa Resuelta JsonBin");
+            return data.record;
+        } catch (error) {
+            console.error("Error cargando el json:" + error);
+            await new Promise(resolve => setTimeout(resolve, 3000));
+        }
+    }
+};
+window.articlesPromise = getArticlesPromise();
+
+document.addEventListener("DOMContentLoaded", () => {
+
+// Plantilla HTML para productos
+const htmlArticle = (img,d,a,t) => {
+    return `<div ${a} class="articulo">
+    <img class="articulo__img" src="${img}">
+    <h3 class='articulo__title'>${t}</h3>
+    <p class="articulo__p">${d}</p>
+    </div>`;
+};
+
+// Eliminar producto de la API
+const deleteArticle = async title => {
+    try {
+        let articleOriginal = await articlesPromise;
+        let articleUpdated = articleOriginal.filter(e => e.title !== title);
+        
+        const response = await fetch(`https://api.jsonbin.io/v3/b/${keyBin}`, {
+            method: 'PUT',
+            headers: {
+                'X-Master-Key': xMasterKey,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(articleUpdated)
+        });
+        
+        if (!response.ok) {
+            formAlert.innerText = "Error al eliminar el artículo";
+            throw new ConectionError("Error al eliminar de la BD el artículo");
+        }
+        
+        // Eliminar elementos del DOM
+        document.querySelectorAll(`[data-title="${title}"]`).forEach(el => {
+            el.parentElement.parentElement.remove();
+        });
+        console.log("eliminado con exito");
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+const showArticles = async () => {
+        const articles = await articlesPromise;
+        let articleGeneral = "";
+        let itemListElement = [];
+        const schema = {
+        "@context": "https://schema.org",
+        "@type": "ItemList",
+        "itemListElement": itemListElement
+        };
+
+        // Generar HTML para cada producto
+        articles.forEach((p,i) => {
+            let article = htmlArticle(p.img, p.description, p.author,p.title);
+            
+            articleGeneral += article ;
+            
+            
+            //alamcenar schema de productos
+            itemListElement.push({
+                "@context": "https://schema.org",
+              "@type": "BlogPosting",
+              "headline": "Título del artículo",
+              "description": "Descripción breve del artículo (meta descripción, 150-160 caracteres).",
+              "author": {
+                "@type": "Person",
+                "name": p.author,
+                "url": "https://yoandri.vercel.app/blog-page.html"
+              },
+              "datePublished": new Date(Date.now() - 86400000).toLocaleDateString('en-CA'),
+              "dateModified": new Date().toLocaleDateString('en-CA'),
+              "image": {
+                "@type": "ImageObject",
+                "url": p.img,
+                "width": 1200,
+                "height": 630
+              },
+              "publisher": {
+                "@type": "Organization",
+                "name": "El Blog de Yoandri",
+                "logo": {
+                  "@type": "ImageObject",
+                  "url": "https://yoandri.vercel.app/recursos/LDY_portada.jpg",
+                  "width": 600,
+                  "height": 60
+                }
+              },
+              "mainEntityOfPage": {
+                "@type": "WebPage",
+                "@id": "https://yoandri.vercel.app/blog-page.html"
+                }
+                });
+            });
+        
+
+        try {
+            
+            document.querySelector("#blog").innerHTML = articleGeneral;
+            //inyectar schema en el body
+            const script = document.createElement('script');
+            script.type = 'application/ld+json';
+            script.textContent = JSON.stringify(schema);
+            document.head.appendChild(script);
+            
+            
+
+            // Agregar listeners de eliminación (solo admin)
+            /* 
+            if (isAdmin) {
+                document.querySelectorAll(".btn-delete").forEach(btn => {
+                    btn.addEventListener("click", e => {
+                        const title = e.target.dataset.title;
+                        deleteArticle(title);
+                    });
+                }); 
+            } 
+            */
+            
+  
+  
+        } catch (e) {
+            console.log("Error al mostrar articulos:" + e);
+        }
+    };
+
+    showArticles();
+    
+    
+    
+});
---
+++ js/dashboard.js
@@ -0,0 +1,132 @@
+const formAlert = document.querySelector(".form-alert");
+const loadingBlog = document.querySelector(".loading-blog");
+const formBlog = document.querySelector(".form-blog");
+const converter = new showdown.Converter();
+
+
+//Function Comprimir Imagenes a 70KB
+async function comprimirA70KB(imagen) {
+    const opciones = {
+        maxSizeMB: 0.06,
+        useWebWorker: true,
+        maxWidthOrHeight: 1920,
+        fileType: 'image/png'
+    };
+
+    try {
+        const imagenComprimida = await imageCompression(imagen, opciones);
+        console.log('[Compresión] Resultado:', {
+            pesoOriginal: `${(imagen.size / 1024).toFixed(2)} KB`,
+            pesoComprimido: `${(imagenComprimida.size / 1024).toFixed(2)} KB`,
+            reduccion: `${((1 - (imagenComprimida.size / imagen.size))) * 100}`
+        });
+        return imagenComprimida;
+    } catch (error) {
+        console.error('[Compresión] Error:', {
+            error: error.message,
+            archivo: imagen.name,
+            tipo: imagen.type
+        });
+        throw new Error('Error en compresión de imagen. Usando original');
+    }
+}
+
+// Function Comprimir-ImgBB-JSONBin
+async function uploadArticle() {
+    try {
+    formAlert.innerHTML = "";
+    let errorMessage = "";
+        // Obtener valores del formulario
+        const elements = {
+            image: document.querySelector(".input-image"),
+            title: document.querySelector(".input-title"),
+            description: document.querySelector(".input-description"),
+            author: document.querySelector(".input-author"),
+        };
+
+        // Validaciones básicas
+        if (!elements.image.files[0]) {
+            errorMessage = "❌ Inserte una imagen";
+            throw new Error('Imagen no seleccionada');
+        }
+        if (!elements.description.value.trim()) {
+            errorMessage = "Escriba una descripción";
+            throw new Error('No hay descripción');
+        }
+
+        loadingBlog.style.display = "block";
+        formBlog.querySelector('button[type="submit"]').style.display = "none";
+
+        // Proceso de compresión
+        const compressedFile = await comprimirA70KB(elements.image.files[0]);
+        
+        // Subir imagen a ImgBB
+        const formData = new FormData();
+        formData.append("image", compressedFile);
+        
+        const responseImgBb = await fetch(`https://api.imgbb.com/1/upload?key=${keyImgBb}`, {
+            method: "POST",
+            body: formData
+        });
+        
+        const dataImgBb = await responseImgBb.json();
+        if (!dataImgBb.success) {
+            console.error('[ImgBB] Error en respuesta:', dataImgBb);
+            throw new Error(`API ImgBB: ${dataImgBb.error?.message || 'Error desconocido'}`);
+        }
+
+        // Actualizar JSON con nuevo producto
+        const articles = await articlesPromise;
+        const newArticle = {
+            img: dataImgBb.data.url,
+            title:elements.title.value,
+            description: DOMPurify.sanitize(converter.makeHtml(elements.description.value)),
+            author:elements.author.value
+        };
+        
+        const response = await fetch(`https://api.jsonbin.io/v3/b/${keyBin}`, {
+            method: 'PUT',
+            headers: {
+                'X-Master-Key': xMasterKey,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify([...articles, newArticle])
+        });
+        
+        if (!response.ok) {
+            const errorData = await response.json();
+            console.error('[JSONBin] Error:', {
+                status: response.status,
+                error: errorData
+            });
+            throw new Error(`API JSONBin: ${errorData.message || 'Error en actualización'}`);
+        }
+
+        console.log('[Éxito] Artículo publicado:', newArticle);
+        formAlert.innerHTML = "<strong>Artículo subido correctamente</strong>";
+        
+        // Resetear formulario
+        formBlog.reset();
+        window.articlesPromise = getArticlesPromise(); // Actualizar lista
+
+    } catch (error) {
+        console.error(`[Error General] ${error.message}`, {
+            stack: error.stack,
+            cause: error.cause
+        });
+        
+        formAlert.innerHTML = errorMessage || `
+            <strong>⚠️ Error al subir el artículo:</strong><br>
+            ${error.message.replace('API JSONBin: ', '').replace('API ImgBB: ', '')}
+        `;
+        
+    } finally {
+        loadingBlog.style.display = "none";
+        formBlog.querySelector('button[type="submit"]').style.display = "block";
+    }
+}
+
+formBlog.addEventListener("submit",function(event){
+    event.preventDefault();
+    uploadArticle();
+});
